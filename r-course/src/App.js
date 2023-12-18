7d033c450f102e4f40f85f60e18f0f1a00f609b8
JayShriR <navrangerameshwar@gmail.com>
2023-12-18 23:04:14 +0530

    file modified 2

--- r-course/src/App.js
+++ r-course/src/App.js
@@ -1,11 +1,43 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import Cards from './components/Cards';
+import Filter from './components/Filter';
+import Navbar from './components/Navbar';
+import Spinner from './components/Spinner';
+import { apiUrl,filterData } from './data';
+import {toast} from "react-toastify";
+
 
 function App() {
+  const [courses,setCoures]=useState(null);
+  const [loading,setLoading]=useState(true);
+  async function fetchData(){
+    setLoading(true); 
+    try{
+      let response=await fetch(apiUrl);
+      let output=await response.json();
+      setCoures(output);
+    }
+    catch(error){
+      toast.error("Network main koi dikkat hain");
+    }
+    setLoading(false);
+  }
+
+  useEffect(()=>{
+   fetchData(); 
+  },[]);
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <div>
+        <Navbar/>
+      </div>
+      <div>
+        <Filter filterData={filterData}/>
+      </div>
+      <div>  
+        {loading ? (<Spinner/>) : (<Cards courses={courses}/>) }
+      </div>
     </div>
   );
 }
---
+++ r-course/src/components/Cards.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const Cards=(props)=>{
+    let courses=props.courses;
+    console.log("printing data");
+    console.log(courses);
+    return (
+        <div>
+            
+        </div>
+    )
+}
+export default Cards;
